972c02d447b71b5a876dcafd5c01f3576d5a172e
tuananhhoang1805 <81757605+tuananhhoang1805@users.noreply.github.com>
2022-02-16 02:03:34 +0700

    Update Editor

---
+++ src/components/editor/Editor.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ReactMde from 'react-mde'
+import Showdown from "showdown";
+import { Editorsbar } from '../../Style'
+import "react-mde/lib/styles/css/react-mde-all.css";
+import '../../App.css'
+
+
+const Editor = ({currentNote,updateNote }) => {
+    const [ selectedTab , setSelectedTab ] = useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })
+    return (
+        <Editorsbar>
+            <ReactMde
+                value={currentNote}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={markdown =>
+                  Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </Editorsbar>
+    )
+}
+
+export default Editor
