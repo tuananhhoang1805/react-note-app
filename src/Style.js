e9248df64daca76eb6bf113b088b3ab2ea4de061
tuananhhoang1805 <81757605+tuananhhoang1805@users.noreply.github.com>
2022-02-17 03:23:45 +0700

    finish style

--- src/Style.js
+++ src/Style.js
@@ -41,6 +41,58 @@ export const TextSnippet = styled.h4`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+
+   
+`
+
+
+
+export const Trash = styled.i`
+    box-sizing: border-box;
+    position: relative;
+    display: block;
+    transform: scale(var(--ggs,1));
+    width: 10px;
+    height: 12px;
+    border: 2px solid transparent;
+    box-shadow:
+        0 0 0 2px,
+        inset -2px 0 0,
+        inset 2px 0 0;
+    border-bottom-left-radius: 1px;
+    border-bottom-right-radius: 1px;
+    margin-top: 4px;
+
+    &:after
+    &:before {
+        content: "";
+        display: block;
+        box-sizing: border-box;
+        position: absolute
+    }
+    &:after {
+        background: currentColor;
+        border-radius: 3px;
+        width: 16px;
+        height: 2px;
+        top: -4px;
+        left: -5px
+    }
+    &:before {
+        width: 10px;
+        height: 4px;
+        border: 2px solid;
+        border-bottom: transparent;
+        border-top-left-radius: 2px;
+        border-top-right-radius: 2px;
+        top: -7px;
+        left: -2px
+    }
+`
+export const TrashButton = styled.button`
+     display: none;
+     border: none;
+     background: none;
 `
 
 export const Seclect = styled.div`
@@ -49,5 +101,10 @@ export const Seclect = styled.div`
         color : #fff;
     }
     color: #4A4E74;
+
+    &:hover  > ${TrashButton}{
+        display: block;
+    }
     
 `
+
